5de57d62fcbc0640c51901edcddca04cea37feea
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-01-02 11:13:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-   origin: "*",
+   origin: "http://localhost:5173",
    methods: ['GET', 'POST', 'PUT', "DELETE"],
    credentials: true
 }));
